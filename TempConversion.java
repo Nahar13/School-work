a4bccd461492b894cd55c0432fed72548ee6a108
Nahar13 <35702029+Nahar13@users.noreply.github.com>
2018-04-16 11:15:29 -0400

    Add files via upload

--- TempConversion.java
+++ TempConversion.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /*Will Smith
  * CSCI 1301 MW 9AM
  * Lab 2.1 Temperature Conversion
- * This lab will give Temperature in C based on F entered
+ * This lab will give Temperature in Celsius based on Fahrenheit entered
+ * and vice versa
  */
 
 public class TempConversion {
@@ -21,26 +22,37 @@ public class TempConversion {
 		switch (formula) {
 		case  1://Prompt for Celsius.
 			System.out.println("Temperature?");
-			double celsius = input.nextDouble();
+			double c = input.nextDouble();
 			
 			// Conversion Formula for Celsius to Fahrenheit
-			double fahrenheit = ((9.0/5.0) * celsius + 32);
-			System.out.println("The temperature is "+ fahrenheit +"degrees fahrenheit.");
+			double f = ((9.0/5.0) * c + 32);
+			System.out.println("The temperature is "+ f +"degrees fahrenheit.");
 			break;
 			
 		case 2://Prompt for fahrenheit
 			System.out.println("Temperature?");
-			fahrenheit = input.nextDouble();
+			f = input.nextDouble();
 			
 			//Conversion formula for fahrenheit
-			celsius = (5.0/9) *(fahrenheit - 32);
-			System.out.println("The tepmerature is "+ celsius + " degrees celsius.");
+			c = (5.0/9) *(f - 32);
+			System.out.println("The temperature is "+ c + " degrees celsius.");
 			break;
 			
 		default: //Response for invalid input
 			System.out.println("Invalid response");
 		}		
-
+		
+		//Method for C to F
+		public static double celsius(double c) {
+			double f = ((9.0/5.0) * c + 32);
+			System.out.println("The temperature is "+ f +"degrees fahrenheit.");
+		}	
+		//Method for F to C
+		public static double fahrenheit(double f) {
+			c = (5.0/9) *(f - 32);
+			System.out.println("The temperature is "+ c + " degrees celsius.");
+		}
+		
 	}
 
 }
