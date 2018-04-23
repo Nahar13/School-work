f7021264c88b8691993d70875e46f0e933cee844
Nahar13 <35702029+Nahar13@users.noreply.github.com>
2018-04-23 13:19:28 -0400

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -6,6 +6,7 @@ public class Menu {
 
 	public static void main(String[] args) {
 		
+		
 		//scanner
 		Scanner input = new Scanner(System.in);
 		
@@ -20,34 +21,35 @@ public class Menu {
 		int partySize = 0;
 		
 		//Party size input
-		System.out.println("How many people are in the party");
+		System.out.println("How many people are in the party?");
 		partySize = input.nextInt();
 		
 		//Loop to get everyones total
-		for (int i = 0; i == partySize; i++) {
+		for (int i = 0; i < partySize; i++) {
+			
 			//Select discount type here
-			System.out.print("Which age group is the customer in?\n"
-					+"5 years old or younger? Enter 1\n"
-					+"13 to 18 years old? Enter 2\n"
-					+"65 or older? Enter 3\n"
-					+"Not any previous group? Enter 4");
+			System.out.print("Which age group is the customer in? \n"
+					+"5 years old or younger? Enter 1 \n"
+					+"13 to 18 years old? Enter 2 \n"
+					+"65 or older? Enter 3 \n"
+					+"Not any previous group? Enter 4 \n");
 			int discountType = input.nextInt();
 			//reset personTotal
 			personTotal = 0;
 			
 				
 				//For loop to get individual orders
-				for (int x = 1; x == 3; x++) {
+				for (int x = 1; x < 4; x++) {
 					System.out.println("Item ordered number" + x);
-					System.out.print("For" + itemname[0] + "enter 1 \n"
-							+"For" + itemname[1] + "enter 2 \n"
-							+"For" + itemname[2] + "enter 3 \n"
-							+"For" + itemname[3] + "enter 4 \n"
-							+"For" + itemname[4] + "enter 5 \n"
-							+"For" + itemname[5] + "enter 6 \n"
-							+"For" + itemname[6] + "enter 7 \n"
-							+"For" + itemname[7] + "enter 8 \n"
-							+"For" + itemname[8] + "enter 9 \n");
+					System.out.print("For " + itemname[0] + " enter 1 \n"
+							+"For " + itemname[1] + " enter 2 \n"
+							+"For " + itemname[2] + " enter 3 \n"
+							+"For " + itemname[3] + " enter 4 \n"
+							+"For " + itemname[4] + " enter 5 \n"
+							+"For " + itemname[5] + " enter 6 \n"
+							+"For " + itemname[6] + " enter 7 \n"
+							+"For " + itemname[7] + " enter 8 \n"
+							+"For " + itemname[8] + " enter 9 \n");
 					int menu = input.nextInt();
 					
 					//Menu item switch
@@ -95,8 +97,33 @@ public class Menu {
 						System.out.println("Invalid selection.");
 						x--;
 					break;
+					
+					}		
 				}
-			}
+				switch (discountType) {
+				
+				case 1:// 5 or under
+					personTotal = 0;
+				break;
+				
+				case 2://teen. 25% off, no tax
+					personTotal = (personTotal * .75);
+				break;
+				
+				case 3://senior citizen. 25% off, no tax
+					personTotal = (personTotal * .75);
+				break;
+				
+				case 4://regular customer. 5% tax
+					personTotal = (personTotal + (personTotal * .05));
+				break;
+				
+				}
+				grandTotal = grandTotal + personTotal;
+				System.out.println(String.format("This person's total is $" + "%.2f", personTotal ));
+				System.out.println("----------------------------");
 		}
+		System.out.println(String.format("The final total is $" + "%2.2f" + grandTotal));
 	}
 }
+
